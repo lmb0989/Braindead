aecef53fb2b70505f77b1ee41ee1f9b2cdd5cceb
lmb0989 <1150274785@qq.com>
2017-06-27 21:04:56 +0800

    add Base file

---
+++ assets/scripts/Battle/Monster/BaseMonster.js
@@ -0,0 +1,19 @@
+var BaseComponent = require("BaseComponent");
+
+cc.Class({
+    extends: BaseComponent,
+
+    properties: {},
+
+    ctor: function () {
+
+    },
+
+    onLoad: function () {
+
+    },
+
+    update: function (dt) {
+
+    },
+});
